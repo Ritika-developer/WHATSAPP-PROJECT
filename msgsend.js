a341be01bb14c1f4ab427b48de2611de8c5462d4
Ritika-developer <ritikachoudhary6264@gmail.com>
2025-04-18 17:51:50 +0530

    whatsapp

---
+++ msgsend.js
@@ -0,0 +1,54 @@
+
+    let fetchData= async()=>{
+    
+    let url='http://localhost:3000/whatsapp'
+    
+    // let res =await fetch(url)
+    let res =await fetch(url ,{method:"GET"})
+    
+    
+    let data = await res.json()
+    
+    console.log(data);
+    
+    let show = document.getElementById('datashow')
+    data.map((e)=>{
+        show.innerHTML+=`
+          <div class="message my_msg">
+                    <p>${e.name}<br><span> </span></p>
+                </div>
+    
+        `
+    })
+
+    }
+    
+    
+    fetchData()
+    // :- isko body mai dal diya
+    
+    
+    
+    //post method 
+    let book=()=>{
+    let inpname = document.getElementById('inpmsg').value
+    
+    
+    let url = "http://localhost:3000/whatsapp"
+    
+    fetch(url,{
+    method:"POST",
+    headers:{
+    "content-type":"application/json"
+    },
+    body:JSON.stringify(
+    {
+        "name":inpname,
+    
+    }
+    )
+    })
+
+    return false
+    }
+    
